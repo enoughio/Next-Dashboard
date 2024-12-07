562fa63cbb12312f9bcce7cea7823ea304917053
Ani <turboji007@gmail.com>
2024-12-07 15:12:30 +0530

    learned about Navigation

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter,Lusitana } from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin']})
+
+export const lusitana = Lusitana({
+  weight: ["400"], // List all the weights you want to include
+  subsets: ["latin"], // Specify the character subsets, if necessary
+});
