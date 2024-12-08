63093f7bb1763e11986ddc90d45bd027739c5e4e
Ani <turboji007@gmail.com>
2024-12-08 23:28:50 +0530

    made minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         {children}
+        {/* added analytics */}
         <Analytics />
       </body>
     </html>
