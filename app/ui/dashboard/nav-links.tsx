932c8424bb1987314c2a1b4f72aefb28e85a3084
Ani <turboji007@gmail.com>
2025-04-17 17:22:54 +0530

    added client directive

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,29 +1,19 @@
-'use client'
-
+'use client';
+ 
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
-} from "@heroicons/react/24/outline";
-import clsx from "clsx";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
-const links = [
-  { name: "Home", href: "/dashboard", icon: HomeIcon },
-  {
-    name: "Invoices",
-    href: "/dashboard/invoices",
-    icon: DocumentDuplicateIcon,
-  },
-  { name: "Customers", href: "/dashboard/customers", icon: UserGroupIcon },
-];
-
+} from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+ 
+// ...
+ 
 export default function NavLinks() {
   const pathname = usePathname();
-
+ 
   return (
     <>
       {links.map((link) => {
